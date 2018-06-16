5a1d0846ad39f82afdf3265d32171be0f3fa4c9c
yinm <yinm.cminor9@gmail.com>
2018-06-16 10:46:33 +0900

    Update layouts

--- device-info-sample/index.js
+++ device-info-sample/index.js
@@ -1,84 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const ChromeSamples = {
-    log() {
-      const line = Array.prototype.slice.call(arguments).map(argument => {
-        return typeof argument === 'string' ?
-          argument :
-          JSON.stringify(argument)
-      })
-        .join(' ')
+function onButtonClick() {
+  let filters = []
 
-      document.querySelector('#log').textContent += line + '\n'
-    },
-
-    clearLog() {
-      document.querySelector('#log').textContent = ''
-    },
-
-    setStatus(status) {
-      document.querySelector('#status').textContent = status
-    },
-
-    setContent(newContent) {
-      let content = document.querySelector('#content')
-      while (content.hasChildNodes()) {
-        content.removeChild(content.lastChild)
-      }
-      content.appendChild(newContent)
-    }
+  let filterService = document.querySelector('#service').value
+  if (filterService.startsWith('0x')) {
+    filterService = parseInt(filterService)
+  }
+  if (filterService) {
+    filters.push({service: [filterService]})
   }
 
-  const log = ChromeSamples.log
-
-  function onButtonClick() {
-    let filters = []
-
-    let filterService = document.querySelector('#service').value
-    if (filterService.startsWith('0x')) {
-      filterService = parseInt(filterService)
-    }
-    if (filterService) {
-      filters.push({ services: [filterService] })
-    }
-
-    let filterName = document.querySelector('#name').value
-    if (filterName) {
-      filters.push({ name: filterName })
-    }
-
-    let filterNamePrefix = document.querySelector('#namePrefix').value
-    if (filterNamePrefix) {
-      filters.push({ namePrefix: filterNamePrefix })
-    }
-
-    let options = {}
-    // if (document.querySelector('#allDevices').checked) {
-    //   options.acceptAllDevices = true
-    // } else {
-      options.filters = filters
-    // }
-
-    log('Requesting Bluetooth Device...')
-    log(`with ${JSON.stringify(options)}`)
-    navigator.bluetooth.requestDevice(options)
-      .then(device => {
-        log(`> Name: ${device.name}`)
-        log(`> Id: ${device.id}`)
-        log(`> Connected: ${device.gatt.connected}`)
-      })
-      .catch(error => {
-        log(`Argh! ${error}`)
-      })
+  let filterName = document.querySelector('#name').value
+  if (filterName) {
+    filters.push({name: filterName})
   }
 
-  document.querySelector('form').addEventListener('submit', (event) => {
-    event.stopPropagation()
-    event.preventDefault()
+  let filterNamePrefix = document.querySelector('#namePrefix').value
+  if (filterNamePrefix) {
+    filters.push({namePrefix: filterNamePrefix})
+  }
 
-    ChromeSamples.clearLog()
-    onButtonClick()
-  })
+  let options = {}
+  if (document.querySelector('#allDevices').checked) {
+    options.acceptAllDevices = true
+  } else {
+    options.filters = filters
+  }
 
-
-}, false)
-
+  log('Requesting Bluetooth Device...')
+  log(`with ${JSON.stringify(options)}`)
+  navigator.bluetooth.requestDevice(options)
+    .then((device) => {
+      log(`> Name: ${device.name}`)
+      log(`> Id: ${device.id}`)
+      log(`> Connected: ${device.gatt.connected}`)
+    })
+    .catch((error) => {
+      log(`Argh! ${error}`)
+    })
+}
