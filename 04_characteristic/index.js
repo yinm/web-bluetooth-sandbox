2b2ebb6aa845e65e8afedee54869f9846b593961
yinm <yinm.cminor9@gmail.com>
2018-06-18 09:16:38 +0900

    Check async/await

--- 04_characteristic/index.js
+++ 04_characteristic/index.js
@@ -1,4 +1,4 @@
-function onButtonClick() {
+async function onButtonClick() {
   let serviceUuid = document.querySelector('#service').value
   if (serviceUuid.startsWith('0x')) {
     serviceUuid = parseInt(serviceUuid)
@@ -9,39 +9,35 @@ function onButtonClick() {
     characteristicUuid = parseInt(characteristicUuid)
   }
 
-  log('Requesting Bluetooth Device...')
-  navigator.bluetooth.requestDevice({
-    filters: [
-      { services: [serviceUuid] },
-      { namePrefix: 'konashi' },
-    ]
-  })
-    .then(device => {
-      log('Connecting to GATT Server...')
-      return device.gatt.connect()
+  try {
+    log('Requesting Bluetooth Device...')
+    const device = await navigator.bluetooth.requestDevice({
+      filters: [
+        { services: [serviceUuid] },
+        { namePrefix: 'konashi' },
+      ]
     })
-    .then(server => {
-      log('Getting Service...')
-      return server.getPrimaryService(serviceUuid)
-    })
-    .then(service => {
-      log('Getting Characteristic...')
-      return service.getCharacteristic(characteristicUuid)
-    })
-    .then(characteristic => {
-      log(`> Characteristic UUID: ${characteristic.uuid}`)
-      log(`> Broadcast: ${characteristic.properties.broadcast}`)
-      log(`> Read: ${characteristic.properties.read}`)
-      log(`> Write w/o response: ${characteristic.properties.writeWithoutResponse}`)
-      log(`> Write: ${characteristic.properties.write}`)
-      log(`> Notify: ${characteristic.properties.notify}`)
-      log(`> Indicate: ${characteristic.properties.indicate}`)
-      log(`> Signed Write: ${characteristic.properties.authenticatedSignedWrites}`)
-      log(`> Queued Write: ${characteristic.properties.reliableWrite}`)
-      log(`> Writable Auxiliaries: ${characteristic.properties.writableAuxiliaries}`)
-      console.log(characteristic)
-    })
-    .catch(error => {
-      log(`Argh! ${error}`)
-    })
+
+    log('Connecting to GATT Server...')
+    const server = await device.gatt.connect()
+
+    log('Getting Service...')
+    const service = await server.getPrimaryService(serviceUuid)
+
+    log('Getting Characteristic...')
+    const characteristic = await service.getCharacteristic(characteristicUuid)
+
+    log(`> Characteristic UUID: ${characteristic.uuid}`)
+    log(`> Broadcast: ${characteristic.properties.broadcast}`)
+    log(`> Read: ${characteristic.properties.read}`)
+    log(`> Write w/o response: ${characteristic.properties.writeWithoutResponse}`)
+    log(`> Write: ${characteristic.properties.write}`)
+    log(`> Notify: ${characteristic.properties.notify}`)
+    log(`> Indicate: ${characteristic.properties.indicate}`)
+    log(`> Signed Write: ${characteristic.properties.authenticatedSignedWrites}`)
+    log(`> Queued Write: ${characteristic.properties.reliableWrite}`)
+    log(`> Writable Auxiliaries: ${characteristic.properties.writableAuxiliaries}`)
+  } catch(error) {
+    log(`Argh! ${error}`)
+  }
 }
