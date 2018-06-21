e38baebb020680c69cfe73077dcede65d1627f8d
yinm <yinm.cminor9@gmail.com>
2018-06-21 09:42:24 +0900

    Check async/await ver

--- 07/index.js
+++ 07/index.js
@@ -1,4 +1,4 @@
-function onButtonClick() {
+async function onButtonClick() {
   let serviceUuid = document.querySelector('#service').value
   if (serviceUuid.startsWith('0x')) {
     serviceUuid = parseInt(serviceUuid)
@@ -9,34 +9,31 @@ function onButtonClick() {
     characteristicUuid = parseInt(characteristicUuid)
   }
 
-  log('Requesting Bluetooth Device...')
-  navigator.bluetooth.requestDevice({
-    filters: [
-      {services: [serviceUuid]},
-      {namePrefix: 'konashi'},
-    ]
-  })
-    .then(device => {
-      log('Connecting to GATT Server...')
-      return device.gatt.connect()
+  try {
+    log('Requesting Bluetooth Device...')
+    const device = await navigator.bluetooth.requestDevice({
+      filters: [
+        {services: [serviceUuid]},
+        {namePrefix: 'konashi'},
+      ]
     })
-    .then(server => {
-      log('Getting Service...')
-      return server.getPrimaryService(serviceUuid)
-    })
-    .then(service => {
-      log('Getting Characteristics...')
-      if (characteristicUuid) {
-        // Get all characteristics that match this UUID.
-        return service.getCharacteristics(characteristicUuid)
-      }
-      // Get all characteristics.
-      return service.getCharacteristics()
-    })
-    .then(characteristics => {
-      log(`> Characteristics: ${characteristics.map(c => c.uuid).join('\n' + ' '.repeat(19))}`)
-    })
-    .catch(error => {
-      log(`Argh! ${error}`)
-    })
+
+    log('Connecting to GATT Server...')
+    const server = await device.gatt.connect()
+
+    log('Getting Service...')
+    const service = await server.getPrimaryService(serviceUuid)
+
+    log('Getting Characteristics...')
+    let characteristics
+    if (characteristicUuid) {
+      characteristics = await service.getCharacteristics(characteristicUuid)
+    } else {
+      characteristics = await service.getCharacteristics()
+    }
+
+    log(`> Characteristics: ${characteristics.map(c => c.uuid).join('\n' + ' '.repeat(19))}`)
+  } catch(error) {
+    log(`Argh! ${error}`)
+  }
 }
